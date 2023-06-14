b4f51992534098d4059d6b6d94414fb0fd3df800
Alexander Grishkin <grinal82@gmail.com>
2023-06-14 22:25:16 +0300

    modified to ensure one click per one increment of dead/lost count

--- src/js/app.js
+++ src/js/app.js
@@ -5,23 +5,27 @@ let idArray = document.querySelectorAll(".hole");
 
 for (let el of idArray) {
   let getHole = document.getElementById(el.id);
-  getHole.onclick = () => {
-    if (getHole.className.includes("hole_active")) {
+  let clicked = false; // Flag to track if the hole has been clicked
+
+  getHole.addEventListener("click", () => {
+    if (getHole.className.includes("hole_active") && !clicked) {
       if (Number(dead.textContent) < 8) {
         dead.textContent = Number(dead.textContent) + 1;
+        clicked = true; // Set clicked flag to true
       } else {
         alert("You have won!!!");
         dead.textContent = 0;
         lost.textContent = 0;
       }
-    } else if (getHole.className.includes("hole")) {
+    } else if (getHole.className.includes("hole") && !clicked) {
       if (Number(lost.textContent) < 4) {
         lost.textContent = Number(lost.textContent) + 1;
+        clicked = true;
       } else {
-        alert("Sorry, buy You've LOST!\nTry again!");
+        alert("Sorry, but you've LOST!\nTry again!");
         dead.textContent = 0;
         lost.textContent = 0;
       }
     }
-  };
+  });
 }
