56e217ba21dbb0af75e86611900939d2809f92f9
Alexander Grishkin <grinal82@gmail.com>
2023-06-14 22:27:06 +0300

    comments added to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,13 @@ let idArray = document.querySelectorAll(".hole");
 
 for (let el of idArray) {
   let getHole = document.getElementById(el.id);
-  let clicked = false; // Flag to track if the hole has been clicked
+  let clicked = false; // флаг чтобы отслеживать что не было клика
 
   getHole.addEventListener("click", () => {
     if (getHole.className.includes("hole_active") && !clicked) {
       if (Number(dead.textContent) < 8) {
         dead.textContent = Number(dead.textContent) + 1;
-        clicked = true; // Set clicked flag to true
+        clicked = true; // переводим флаг в true
       } else {
         alert("You have won!!!");
         dead.textContent = 0;
@@ -20,7 +20,7 @@ for (let el of idArray) {
     } else if (getHole.className.includes("hole") && !clicked) {
       if (Number(lost.textContent) < 4) {
         lost.textContent = Number(lost.textContent) + 1;
-        clicked = true;
+        clicked = true; // переводим флаг в true
       } else {
         alert("Sorry, but you've LOST!\nTry again!");
         dead.textContent = 0;
